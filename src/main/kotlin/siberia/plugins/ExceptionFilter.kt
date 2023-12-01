aa2fc949d36185b0ba3dae50a5da3a6167045665
dudosyka <a.hatson22@gmail.com>
2023-12-02 01:57:59 +0300

    feat(internal): Custom logger wrapper

--- src/main/kotlin/siberia/plugins/ExceptionFilter.kt
+++ src/main/kotlin/siberia/plugins/ExceptionFilter.kt
@@ -4,30 +4,20 @@ import io.ktor.client.call.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
-import io.ktor.util.logging.*
 import org.jetbrains.exposed.exceptions.ExposedSQLException
 import siberia.conf.AppConf
 import siberia.exceptions.BaseException
 import siberia.exceptions.InternalServerException
 
 fun Application.configureExceptionFilter() {
-    val logger = mutableMapOf(
-        "main" to KtorSimpleLogger("siberia.ExceptionFilter"),
-        "database" to KtorSimpleLogger("siberia.ExceptionFilter.Database"),
-        "Transformation" to KtorSimpleLogger("siberia.ExceptionFilter.Transformation")
-    )
+
     install(StatusPages) {
-        fun log(call: ApplicationCall, cause: Throwable, prefix: String = "main") {
-            (logger[prefix] ?: logger["main"])!!.info("Request ${call.request.path()} was failed due to $cause")
-            (logger[prefix] ?: logger["main"])!!.debug("Stacktrace => ${cause.stackTraceToString()}")
-        }
         fun Throwable.getClientMessage(): String = if (AppConf.isDebug) message.toString() else ""
 
         exception<Throwable> {
             call, cause ->
-                log(call, cause)
+                Logger.callFailed(call, cause)
                 call.respond<InternalServerException>(
                     HttpStatusCode.InternalServerError,
                     InternalServerException(cause.getClientMessage())
@@ -36,7 +26,7 @@ fun Application.configureExceptionFilter() {
 
         exception<ExposedSQLException> {
             call, exposedSqlException ->
-                log(call, exposedSqlException, "Database")
+                Logger.callFailed(call, exposedSqlException, "Database")
                 call.respond<InternalServerException>(
                     HttpStatusCode.InternalServerError,
                     InternalServerException(exposedSqlException.getClientMessage())
@@ -45,7 +35,7 @@ fun Application.configureExceptionFilter() {
 
         exception<NoTransformationFoundException> {
             call, requestValidationException ->
-                log(call, requestValidationException)
+                Logger.callFailed(call, requestValidationException)
                 call.respond(
                     status = HttpStatusCode.InternalServerError,
                     message = InternalServerException(requestValidationException.getClientMessage())
@@ -54,7 +44,7 @@ fun Application.configureExceptionFilter() {
 
         exception<BaseException> {
             call, cause ->
-                log(call, cause)
+                Logger.callFailed(call, cause)
                 call.respond(
                     status = HttpStatusCode(cause.httpStatusCode, cause.httpStatusText),
                     cause
---
+++ src/main/kotlin/siberia/plugins/Logger.kt
@@ -0,0 +1,22 @@
+package siberia.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.util.logging.*
+
+object Logger {
+    private val logger = mutableMapOf(
+        "main" to KtorSimpleLogger("siberia.ExceptionFilter"),
+        "database" to KtorSimpleLogger("siberia.ExceptionFilter.Database"),
+        "Transformation" to KtorSimpleLogger("siberia.ExceptionFilter.Transformation")
+    )
+
+    fun callFailed(call: ApplicationCall, cause: Throwable, prefix: String = "main") {
+        (logger[prefix].apply { if (this == null) logger["main"]!!.debug("Logger $prefix not found") } ?: logger["main"])!!.info("Request ${call.request.path()} was failed due to $cause")
+        (logger[prefix] ?: logger["main"])!!.debug("Stacktrace => ${cause.stackTraceToString()}")
+    }
+
+    fun debug(message: Any?, prefix: String) {
+        (logger[prefix].apply { if (this == null) logger["main"]!!.debug("Logger $prefix not found") } ?: logger["main"])!!.debug(message.toString())
+    }
+}
