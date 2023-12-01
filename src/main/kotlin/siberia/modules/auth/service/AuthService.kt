1ab041fc37fbe28bdf12eb5d6c213accda0b273a
dudosyka <a.hatson22@gmail.com>
2023-12-02 02:43:24 +0300

    feat(internal): rules and roles managing are moved into UserService

--- src/main/kotlin/siberia/modules/auth/service/AuthService.kt
+++ src/main/kotlin/siberia/modules/auth/service/AuthService.kt
@@ -3,25 +3,23 @@ package siberia.modules.auth.service
 import io.ktor.util.date.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.kodein.di.DI
+import org.kodein.di.instance
 import siberia.exceptions.BadRequestException
 import siberia.exceptions.ForbiddenException
 import siberia.exceptions.UnauthorizedException
-import siberia.exceptions.ValidateException
-import siberia.modules.auth.data.dao.RoleDao
-import siberia.modules.auth.data.dao.RuleDao
-import siberia.modules.auth.data.dao.UserToRuleDao
 import siberia.modules.auth.data.dto.authorization.AuthInputDto
 import siberia.modules.auth.data.dto.authorization.CreateUserInputDto
 import siberia.modules.auth.data.dto.authorization.RefreshTokenDto
 import siberia.modules.auth.data.dto.authorization.TokenOutputDto
-import siberia.modules.stock.data.dao.StockDao
 import siberia.modules.user.data.dao.UserDao
 import siberia.modules.user.data.models.UserModel
+import siberia.modules.user.service.UserService
 import siberia.utils.kodein.KodeinService
 import siberia.utils.security.bcrypt.CryptoUtil
 import siberia.utils.security.jwt.JwtUtil
 
 class AuthService(override val di: DI) : KodeinService(di) {
+    private val userService: UserService by instance()
 
     private fun generateTokenPair(userDao: UserDao): TokenOutputDto {
         val accessToken = JwtUtil.createToken(userDao)
@@ -62,29 +60,6 @@ class AuthService(override val di: DI) : KodeinService(di) {
         generateTokenPair(userDao)
     }
 
-    private fun addRuleToUser(userDao: UserDao, ruleId: Int, stockId: Int? = null) {
-        val ruleDao = RuleDao[ruleId]
-        UserToRuleDao.new {
-            user = userDao
-            rule = ruleDao
-            if (rule.needStock) {
-                if (stockId != null)
-                    stock = StockDao[stockId]
-                else
-                    throw ValidateException.build {
-                        addError(ValidateException.ValidateError("stock_id", "must be provided"))
-                    }
-            }
-        }
-    }
-
-    private fun addRoleToUser(userDao: UserDao, roleId: Int) {
-        val role = RoleDao[roleId]
-        role.outputWithChildren.rules.forEach {
-            addRuleToUser(userDao, it.ruleId, it.stockId)
-        }
-    }
-
     fun signUp(createUserInputDto: CreateUserInputDto): TokenOutputDto = transaction {
         val search = UserDao.find {
             UserModel.login eq createUserInputDto.login
@@ -101,11 +76,11 @@ class AuthService(override val di: DI) : KodeinService(di) {
         try {
 
             createUserInputDto.rules.forEach {
-                addRuleToUser(userDao, it.ruleId, it.stockId)
+                userService.addRuleToUser(userDao, it.ruleId, it.stockId)
             }
 
             createUserInputDto.roles.forEach {
-                addRoleToUser(userDao, it)
+                userService.addRoleToUser(userDao, it)
             }
 
         } catch (e: Exception) {
